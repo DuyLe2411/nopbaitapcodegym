ab5ee45ed79e07069ac045bec7cbab2807fa6d18
QUOCDUY <lequocduy2411@gmail .com>
2021-08-03 23:38:47 +0700

    Add displayinformation.py file

---
+++ displayinformation.py
@@ -0,0 +1,4 @@
+print("Họ và tên: Lê Quốc Duy\n")
+print("Ngày sinh: 24/11/2003\n")
+print("Địa chỉ: K110/33 Phan Thanh, Đà Nẵng\n")
+print("Mục đích:- Trao dồi kỹ năng lập trình, chuẩn bị cho ngành học tương lai.")
