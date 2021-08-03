9a5c8096e10574c55549eb1ad31405001dfe3424
QUOCDUY <lequocduy2411@gmail .com>
2021-08-03 17:55:43 +0700

    Add circledraw.py file

---
+++ circledraw.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.speed(10)
+turtle.pensize(5)
+turtle.pencolor("red")
+turtle.Turtle().goto(40,-120)
+
+turtle.fillcolor("blue")
+turtle.begin_fill()
+
+turtle.circle(100)
+turtle.end_fill()
+turtle.done()
+
