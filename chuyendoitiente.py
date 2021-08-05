df37a9e28b1daad50d4465f24a2e347cacb2833e
QUOCDUY <lequocduy2411@gmail .com>
2021-08-05 16:33:17 +0700

    Add chuyendoitiente.py file

---
+++ chuyendoitiente.py
@@ -0,0 +1,5 @@
+USD = float(input("Số tiền USD mà quý khách muốn đổi là bao nhiêu ạ: "))
+Tỉ_giá = 23000
+VND = USD * Tỉ_giá
+print(f"Số tiền Việt Nam Đồng bạn nhận được sau khi đổi là: {VND}")
+print(f"Số tiền USD bạn muốn đổi: {USD}")
