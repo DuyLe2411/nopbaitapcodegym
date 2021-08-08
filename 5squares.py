e7d697188f5bdbdf95dac4344930efccfc679817
QUOCDUY <lequocduy2411@gmail .com>
2021-08-08 18:46:49 +0700

    5 hinh vuong phien ban nang cap

--- 5squares.py
+++ 5squares.py
@@ -1,3 +1,5 @@
+
+
 import turtle
 t = turtle.Turtle()
 
@@ -7,10 +9,13 @@ t.pensize(5)
 t.pencolor("skyblue")
 t.speed(2)
 for i in range(5):
+    t.lt(100)
     for j in range(4):
         t.lt(90)
-        t.fd(100)
-    t.lt(72)
+        t.fd(200)
+        
+    t.lt(188)
     
 turtle.done()
+
 
