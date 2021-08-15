4ffa3cbcb7504e3433d2401ccc5caf3616f344b4
QUOCDUY <lequocduy2411@gmail .com>
2021-08-15 19:58:36 +0700

    Đếm từ trong chuỗi

---
+++ demtutrongchuoi.py
@@ -0,0 +1,13 @@
+message = '''This is a common message for people, message is private and circulate this with caution'''
+
+def count_words(word):
+    
+    mes = message.split()
+    num = mes.count(word)
+    if word in mes:
+        print(f"Số lần từ {word} trong chuỗi là: {num}" )
+    else:
+        print("Không có từ này")
+
+word = input("Nhập từ bạn muốn tìm trong chuỗi: ")
+count_words(word)
