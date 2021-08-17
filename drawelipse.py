8b9f204bec4672364f0324ac49465796f9fd1df7
QUOCDUY <lequocduy2411@gmail .com>
2021-08-17 19:21:34 +0700

    Bản nâng cấp của vẽ elipse

--- drawelipse.py
+++ drawelipse.py
@@ -1,17 +1,20 @@
 angle = int(input("Nhập độ lệch góc bạn chọn: "))
 import turtle 
+import random as r
 t = turtle.Turtle()
 turtle.title("ELIPSE")
 t.hideturtle()
 turtle.bgcolor("black")
-t.pencolor("yellow")
-t.speed(0)
 
+t.speed(0)
+color_list = ["green","purple","light blue","sky blue","light green","red","pink","yellow","brown","silver"]
 ran = int(360/angle)
 if 360%angle == 0:
     rad = int(input("Độ cong của hình elipse: "))
     for i in range(ran):
         t.rt(angle)
+        color = r.choice(list(color_list))
+        t.pencolor(color)
         for i in range(2):
             t.circle(rad,90)
             t.circle(rad//2,90)
